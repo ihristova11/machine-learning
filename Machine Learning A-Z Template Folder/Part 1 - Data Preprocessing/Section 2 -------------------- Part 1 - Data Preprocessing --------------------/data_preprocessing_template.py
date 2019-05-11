5ee230008277ac71d47e1a72e49dc34c72e905ba
ihristova11 <hristova119@gmail.com>
2019-05-11 18:05:29 +0300

    working with missing data using the mean approach

---
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -0,0 +1,10 @@
+# Data Preprocessing Templates
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+#we should specify the working directory
